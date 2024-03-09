53110ca97e745dc782f97136a2df827f109715ff
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2024-03-09 16:44:14 +0500

    Add files via upload

---
+++ Day02.js
@@ -0,0 +1,13 @@
+//  DAY 02
+
+// HOW TO FIND MISSING NUMBER BETWEEN INTEGER 
+
+// var numbers = [1,2,3,4,5,6,7,8,10];
+
+// for (var i = 0; i < numbers.length ; i++) {
+//     if (numbers[i + 1] - numbers[i] > 1 && numbers[i + 1] - numbers[i] !== 0) {
+//         console.log("Missing number:", numbers[i] + 1);
+//     } else {
+//         console.log("Number is not missing", numbers[i]);
+//     }
+// }
