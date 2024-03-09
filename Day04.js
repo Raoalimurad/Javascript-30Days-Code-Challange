024d2101f6b36dd78f306309ce42c477673755ab
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2024-03-09 16:46:20 +0500

    Add files via upload

---
+++ Day04.js
@@ -0,0 +1,18 @@
+// DAY 04
+// METHOD 01
+// var numbers = [1,2,4,5,6,78,9,7,79];
+// var sum = 0
+
+// for(var i = 0; i<numbers.length;i++){
+//     sum+=numbers[i]
+// }
+// console.log(`the sum of all array numbers is ${sum}`)
+
+
+// // METHOD 02
+// var add =(arr)=>{
+// return arr.reduce((previous,current)=>{
+//     return previous+current
+// })
+// }
+// console.log(add(numbers))
