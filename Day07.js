8e771516f5c65555256317e8e054335d230f8ca0
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2024-03-09 16:49:56 +0500

    Add files via upload

---
+++ Day07.js
@@ -0,0 +1,18 @@
+// DAY O7
+
+// HOW TO FIND VOWELS IN A STRING 
+
+// var user = "RaoAlimurad";
+// var lowercaseName = user.toLowerCase();
+// var findVowel = lowercaseName.split("");
+// var vowel = [];
+
+// for(var i = 0; i < findVowel.length; i++) {
+//     if(findVowel[i] == "a" || findVowel[i] == "e" || findVowel[i] == "i" || findVowel[i] == "o" || findVowel[i] == "u") {
+//         if (!vowel.includes(findVowel[i])) {
+//             vowel.push(findVowel[i]);
+//         }
+//     }
+// }
+
+// console.log(vowel);
