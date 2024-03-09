6afa09ba0ef97dd5ce9d68934e12f24e7ce8d790
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2024-03-09 16:51:45 +0500

    Add files via upload

---
+++ Day09.js
@@ -0,0 +1,13 @@
+
+// DAY 09
+
+// HOW TO FIND FACTORS  OF ANY INTEGER NUMBER IN JAVASCRIPT
+
+// var input = +prompt("Enter any integer value")
+// var factors = []
+// for(var i = 0; i<=input;i++){
+//     if(input%i==0){
+//        factors.push(i)
+//     }
+// }
+// console.log(factors)
