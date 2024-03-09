b1471f6784345ef7cc475eb92a0adbc78d2f5bee
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2024-03-09 16:50:39 +0500

    Add files via upload

---
+++ Day08.js
@@ -0,0 +1,12 @@
+// DAY 08
+
+// HOW TO FIND PALINDROME IN JAVASCRIPT
+// var userInput = prompt('Enter your value');
+// var convertIntoArray = userInput.split("");
+// var reverseArray = convertIntoArray.reverse();
+// var convertIntoString = reverseArray.join("");
+// if (userInput === convertIntoString) {
+//     console.log("this is palindrome")
+// } else {
+//     console.log("this is not  palindrome")
+// }
