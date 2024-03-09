4ee9d3c15e9d11d91b3c2c5c60840b4344150ca3
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2024-03-09 16:48:52 +0500

    Add files via upload

---
+++ Day06.js
@@ -0,0 +1,13 @@
+// DAY O6
+
+
+// HOW TO REVERSE STRING IN A JAVASCRIPT
+
+// function reverseString(arr) {
+//     let convertIntoArray = arr.split("")
+//     let reverseArray = convertIntoArray.reverse();
+//     let convertIntoString = reverseArray.join("")
+//     return convertIntoString;
+
+// }
+// console.log(reverseString('raoali'))
