fdc42d44e736593c0b448ceeafdcb3fd382b9b91
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2024-03-09 16:41:03 +0500

    Add files via upload

---
+++ Day01.js
@@ -0,0 +1,15 @@
+// //  QNO:01 HOW TO FIND DUPLICATE ELEMENT IN A ARRAY?
+// // METHOD NO 1:
+
+// var numbers = [1, 2, 3, 1, 4, 5, 6, 4, 5, 1];
+// var addnumber = [];
+
+// for (var i = 0; i < numbers.length; i++) {
+//     for (var j = i + 1; j < numbers.length; j++) {
+//         if (numbers[i] === numbers[j]) {
+//             addnumber.push(numbers[i]);
+//         }
+//     }
+// }
+
+// console.log(addnumber); 
