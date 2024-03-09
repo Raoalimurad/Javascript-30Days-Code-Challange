675bc21a348f170b24911684258598bba92f122f
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2024-03-09 16:45:27 +0500

    Add files via upload

---
+++ Day03.js
@@ -0,0 +1,18 @@
+// DAY O3
+
+// HOW TO FIND ODD OR EVEN NUMBER 
+
+
+// var numbers = [1,2,3,4,5,6,7,8,9,10];
+// var evenArray = [];
+// var oddArray = [];
+// for(var i =0; i<numbers.length;i++){
+//     if(numbers[i]%2==0){
+//         evenArray.push(numbers[i]);
+//     }else{
+//         oddArray.push(numbers[i]);
+//     }
+// }
+
+// console.log(evenArray);
+// console.log(oddArray);
