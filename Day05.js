e332b207caab7036331702cf36eb43eb1112d7cd
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2024-03-09 16:47:31 +0500

    Add files via upload

---
+++ Day05.js
@@ -0,0 +1,13 @@
+// DAY O5
+// HOW TO FIND FACTORIAL OF ANY INTEGER NUMBER
+
+// var input = +prompt("Enter a number");
+// var fact = 1;
+// if (input > 0) {
+//     for (var i = 1; i <= input; i++) {
+//         fact = fact * i;
+//     }
+// } else {
+//     console.log(`the factorial of this ${input} is not valid`)
+// }
+// console.log(fact)
