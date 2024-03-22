17da38ebfd61c5bc0ba0c8b990506d6852cf2a94
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2024-03-22 12:39:40 +0500

    Add files via upload

---
+++ Day11.js
@@ -0,0 +1,16 @@
+// HOW TO  CEHCK OCCURRENCE OF LETTER IN JAVASCRIPT
+
+// var string = prompt("Enter any string value");
+// var letter = prompt("Enter any letter which you would like to search in string");
+// var strLength = string.length;
+// var count = 0;
+// for (var i = 0; i <= strLength; i++) {
+//     if (string[i] == letter) {
+//         count++;
+//     }
+// }
+// console.log(`In ${string} occurence of ${letter} is ${count}`);
+
+
+
+
